0b1e88a511426d82ed72b3c1e027507dc69cdea3
Azeem01nnie <abdu.mohammadazeem@gmail.com>
2025-04-13 21:39:48 +0800

    Add files via upload

---
+++ AdminDashboard/Ui Admin Optional/modalss.js
@@ -0,0 +1,15 @@
+function openModal() {
+    document.getElementById("logoutModal").style.display = "block";
+  }
+
+  function closeModal() {
+    document.getElementById("logoutModal").style.display = "none";
+  }
+
+  function logout() {
+    // Simulate logout (you can replace this with actual logout logic)
+    alert("You have been logged out.");
+    closeModal();
+    // Redirect to login.html after logout
+    window.location.href = "../marfolder/LogIn.html";
+  }
